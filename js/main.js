c0ff50d14383e34c57341fab11cdf15df9314a2f
VaisakhPradeep <92vaisakh92@gmail.com>
2018-07-07 23:49:49 +0530

    nav functionality and footer

--- js/main.js
+++ js/main.js
@@ -4,5 +4,17 @@ $(document).ready(function(){
 		$(".nav-list").toggleClass("selected");
 	});
 
+	$(".nav-list").click(function(){
+		$(this).toggleClass("selected");
+	});
+
+	$(document).on('click', '.nav', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+	});
+
 
 });
